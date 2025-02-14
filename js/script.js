972a7d4695ae9cdbdc6c3d4757e9c747c2b4ed7e
wltkdgur123@naver.com <wltkdgur123@naver.com>
2025-02-14 17:30:05 +0900

    다중 애니메이션 캐릭터 제어

--- js/script.js
+++ js/script.js
@@ -1,30 +1,68 @@
-const $runningman = document.querySelector('#runningman');
+const $zelda_link = document.querySelector('#zelda_link');
 const characterLocation = {
     x: 0,
     y: 0
-}
+};
 
-const allowedKeys = ['ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown']
-document.addEventListener(
-    'keydown', (event) => {
-        if (!allowedKeys.includes(event.key)) {
-            return;
-        }
-        $runningman.classList.remove('pause-running');
-        $runningman.classList.add('play-running');
-        calcCharacterLocation(event.key);
-        $runningman.setAttribute('style', `transform: translate(${characterLocation.x}px, ${characterLocation.y}px)`);
+const allowedKeys = ['ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown'];
+
+document.addEventListener('keydown', (event) => {
+    if (!allowedKeys.includes(event.key)) {
+        return;
     }
-)
-document.addEventListener(
-    'keyup', (event) => {
-        if (!allowedKeys.includes(event.key)) {
-            return;
-        }
-        $runningman.classList.remove('play-running');
-        $runningman.classList.add('pause-running');
+
+    $zelda_link.classList.remove('pause-moving');
+
+    switch (event.key) {
+        case 'ArrowRight':
+            $zelda_link.classList.remove('play-moving-left', 'play-moving-up', 'play-moving-down');
+            $zelda_link.classList.add('play-moving-right');
+            break;
+        case 'ArrowLeft':
+            $zelda_link.classList.remove('play-moving-right', 'play-moving-up', 'play-moving-down');
+            $zelda_link.classList.add('play-moving-left');
+            break;
+        case 'ArrowUp':
+            $zelda_link.classList.remove('play-moving-right', 'play-moving-left', 'play-moving-down');
+            $zelda_link.classList.add('play-moving-up');
+            break;
+        case 'ArrowDown':
+            $zelda_link.classList.remove('play-moving-right', 'play-moving-left', 'play-moving-up');
+            $zelda_link.classList.add('play-moving-down');
+            break;
     }
-)
+
+    // 위치 업데이트
+    calcCharacterLocation(event.key);
+    $zelda_link.setAttribute('style', `transform: translate(${characterLocation.x}px, ${characterLocation.y}px)`);
+});
+
+document.addEventListener('keyup', (event) => {
+    if (!allowedKeys.includes(event.key)) {
+        return;
+    }
+    console.log(event.key);
+
+
+    switch (event.key) {
+        case 'ArrowRight':
+            $zelda_link.classList.remove('play-moving-right', 'play-moving-left', 'play-moving-up', 'play-moving-down');
+            $zelda_link.classList.add('pause-moving');
+            break;
+        case 'ArrowLeft':
+            $zelda_link.classList.remove('play-moving-right', 'play-moving-left', 'play-moving-up', 'play-moving-down')
+            $zelda_link.classList.add('pause-moving');
+            break;
+        case 'ArrowUp':
+            $zelda_link.classList.remove('play-moving-right', 'play-moving-left', 'play-moving-up', 'play-moving-down');
+            $zelda_link.classList.add('pause-moving');
+            break;
+        case 'ArrowDown':
+            $zelda_link.classList.remove('play-moving-right', 'play-moving-left', 'play-moving-up', 'play-moving-down')
+            $zelda_link.classList.add('pause-moving');
+            break;
+    }
+});
 
 function calcCharacterLocation(key) {
     switch (key) {
